bdf8a2ea3fe337b4a904131f56fe60a3c7f11b5a
KTOINDY <donghappy123@gmail.com>
2019-10-06 17:23:42 +0700

    Add project files.

---
+++ VBAPI/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_register = New System.Windows.Forms.Button()
+        Me.btn_login = New System.Windows.Forms.Button()
+        Me.txt_user = New System.Windows.Forms.TextBox()
+        Me.txt_pass = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn_register
+        '
+        Me.btn_register.Location = New System.Drawing.Point(10, 77)
+        Me.btn_register.Name = "btn_register"
+        Me.btn_register.Size = New System.Drawing.Size(147, 27)
+        Me.btn_register.TabIndex = 0
+        Me.btn_register.Text = "Register"
+        Me.btn_register.UseVisualStyleBackColor = True
+        '
+        'btn_login
+        '
+        Me.btn_login.Location = New System.Drawing.Point(163, 77)
+        Me.btn_login.Name = "btn_login"
+        Me.btn_login.Size = New System.Drawing.Size(147, 27)
+        Me.btn_login.TabIndex = 1
+        Me.btn_login.Text = "Login"
+        Me.btn_login.UseVisualStyleBackColor = True
+        '
+        'txt_user
+        '
+        Me.txt_user.Location = New System.Drawing.Point(71, 25)
+        Me.txt_user.Name = "txt_user"
+        Me.txt_user.Size = New System.Drawing.Size(239, 20)
+        Me.txt_user.TabIndex = 2
+        '
+        'txt_pass
+        '
+        Me.txt_pass.Location = New System.Drawing.Point(71, 51)
+        Me.txt_pass.Name = "txt_pass"
+        Me.txt_pass.Size = New System.Drawing.Size(239, 20)
+        Me.txt_pass.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Password"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(322, 115)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_pass)
+        Me.Controls.Add(Me.txt_user)
+        Me.Controls.Add(Me.btn_login)
+        Me.Controls.Add(Me.btn_register)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_register As Button
+    Friend WithEvents btn_login As Button
+    Friend WithEvents txt_user As TextBox
+    Friend WithEvents txt_pass As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
